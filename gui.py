970aef31a5c3b579bbd007fb02e2fe1b9f221062
황서영 <45020462+hwangsae@users.noreply.github.com>
2018-11-28 21:28:40 +0900

    Update gui.py

--- gui.py
+++ gui.py
@@ -9,26 +9,29 @@ pygame.init()
 # pygame에서 사용할 변수 선언
 BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
-BLUE  = (  0,   0, 255)
-GREEN = ( 47, 157,  39)
+BLUE  = (100, 149, 237)
+GREEN = (  0, 128,   0)
 RED   = (255,   0,   0)
 YELLOW= (255, 187,   0)
-fontObj = pygame.font.SysFont('Arial.ttf', 18)
+SILVER= (192, 192, 192)
+TEAL  = (  0, 128, 128)
 
-pygame.init()
+
 
 # 플레이어 기본 화면 구성
 def window():
-    screen = pygame.display.set_mode([1000, 600])
-    pygame.display.set_caption('Blue Block')
+    screen = pygame.display.set_mode([1000, 600], 0, 32)
+    pygame.display.set_caption('Blue Brick')
     screen.fill(YELLOW)
 
     # 기본 화면 구획
     pygame.draw.rect(screen, WHITE, [660, 70, 310, 500])    # 플레이어 정보 출력 부분
-    pygame.draw.rect(screen, WHITE, [30, 420, 600, 150])    # 금액 입력 부분..?
+    pygame.draw.rect(screen, BLACK, [30, 470, 600, 100])    # 금액 입력 부분..?
+    pygame.draw.rect(screen, WHITE, [28, 468, 604, 104], 4)
     pygame.draw.rect(screen, GREEN, [30, 70, 600, 200])     # 아이템 제시 및 기타 정보
 
     return screen
+
 
 # 플레이어 정보 출력
 class player:
@@ -39,12 +42,28 @@ class player:
         self.turn = turn
 
     # 플레이어의 정보를 띄우는 함수
-    def present(self):
-        pygame.draw.rect(self.screen, BLACK, [690, 90+120*self.turn, 250, 100])
-        textSurfaceObj = fontObj.render(self.name, True, WHITE, BLACK)
-        textRectObj = textSurfaceObj.get_rect()
-        textRectObj.center = (700, 100+120*self.turn)
-        self.screen.blit(textSurfaceObj, textRectObj)
+    def info(self):
+        # 배경 설정
+        pygame.draw.rect(self.screen, SILVER, [680, 90+120*self.turn, 270, 100])
+        # 이미지 출력
+        pygame.draw.rect(self.screen, BLACK, [30+157*self.turn, 378, 130, 90])
+        # 플레이어 이름 출력
+        try:
+            font = pygame.font.Font(None, 18)
+            text = font.render(self.name, True, WHITE, None)
+            textRect = text.get_rect()
+            textRect.center = (700, 100+120*self.turn)
+            self.screen.blit(text, textRect)
+        except:
+            print("no font file")
+            pass
+        # 플레이어 아이템 출력
+
+    # 플레이어가 제시한 금액을 띄우는 함수
+    def take_my_money(self, money):
+        # 금액 출력
+        pass
+
 
 
 # 채팅창(보류)
@@ -74,12 +93,12 @@ if __name__ == '__main__':
         player2 = player(screen, 'foodduck', 1, 0)
         player3 = player(screen, 'yaho', 2, 0)
         player4 = player(screen, 'helpme', 3, 0)
-        player1.present()
-        player2.present()
-        player3.present()
-        player4.present()
+        player1.info()
+        player2.info()
+        player3.info()
+        player4.info()
 
         # 사용자 행위
 
         # 게임 창에 적용
-        pygame.display.flip()
+        pygame.display.update()
