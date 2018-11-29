b61b903bbe973829f61c29fcbab44a02f9d6f72b
Dimen <a4gkyum@naver.com>
2018-11-29 16:37:03 +0900

    Reverted client.py

--- client.py
+++ client.py
@@ -1,237 +1,59 @@
 import socket
-import random
 import threading
-import copy
-from time import sleep
+
+# 접속할 서버의 정보
+server_ip = '127.0.0.1'
+server_port = 50000
+address = (server_ip, server_port)
+
+# 소켓을 이용해서 서버에 접속
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(address)
+
+# 스레드 종료 키
+thread_end = 0
 
 
-SERVER_IP = 'localhost'
-SERVER_PORT = 50000
-SERVER_ADDRESS = (SERVER_IP, SERVER_PORT)
+# 서버가 보내는 메시지를 수신할 함수 | Thread 활용
+def receive():
+    global mysock
 
-# 3초세는거 하기 위한 전역변수
-current_keeper = 0
-call_count = 0
-highest_bidder = None
-is_receiving = True
-
-client_list = []
-client_id = []
-client_class_list = []
-
-START_MONEY = 300
-
-# "Item name": Available Number
-item_dict = {
-    "빨간 벽돌": 6,
-    "파란 벽돌": 1,
-    "나무 합판": 3,
-    "철근": 2,
-    "시멘트": 3,
-    "수령님의 평양냉면": 1,
-    "멸종위기동물 황새": 1
-}
-
-print("*******BLUE BRICK*******")
-print("Waiting for players...({}/4)".format(len(client_list)))
-
-
-class client(threading.Thread):
-
-    def __init__(self, client_socket, client_address, money):
-
-        threading.Thread.__init__(self)
-        self.my_socket = client_socket
-        self.my_address = client_address
-        self.money = money
-        self.items = {}
-        self.name = self.my_socket.fileno()
-        self.is_bankrupt = False
-
-    # 물건을 샀을때 업데이트
-    def update(self, item, price):
-
+    while True:
         try:
-            self.items[item] += 1
-        except KeyError:
-            self.items[item] = 1
+            data = mysock.recv(1024)
+            data = data.decode('UTF-8')
+            if(data == 'end'):
+                mysock.send(bytes("end", 'UTF-8'))
 
-        self.money -= price
-
-    # 클라이언트에게 메세지 보내기
-    def send(self, msg):
-
-        byte_msg = bytes(msg, 'utf-8')
-        self.my_socket.send(byte_msg)
-
-    # 입찰 요청 받고 처리
-    def run(self):
-
-        # 가장 최근에 call한 사용자가 호출한 타이머
-        global call_count
-        # 가장 최근 호출한 사용자
-        global highest_bidder
-
-        while True:
-            try:
-
-                data = self.my_socket.recv(1024)
-                data = data.decode('utf-8')
-                if is_receiving is False:
-                    continue
-
-            except:
-                print("Connection with %d lost!" % (self.name))
-
-            if data == 'CALL':  # 콜을 받았을 경우
-                # 변수 업데이
-                call_count += 1
-                highest_bidder = self.name
-                # 새 타이머 시작
-                # 타이머 이름은 호출 횟수와 같음
-                # 가장 최근에 호출된 타이머를 판별하기 위해
-                new_keeper = timekeeper(5, call_count)
-                new_keeper.start()
-                # 전체에게 메세지 보내기
-                sendMessage(client_list, "{} bid!".format(self.name))
-
-
-# pragma timekeeper
-
-# 타이머 클래스
-class timekeeper(threading.Thread):
-
-    def __init__(self, time, name):
-
-        threading.Thread.__init__(self)
-        self.time = time
-        self.isRunning = True
-        # 타이머에 이름 부여
-        self.name = name
-
-    # 현재 돌아가고 있는 타이머가 본인인지 체크
-
-    def check(self):
-        global call_count
-        # 이 부분이 오류였음
-        return int(call_count) == int(self.name)
-
-    def run(self):
-
-        global is_receiving
-        global client_list
-
-        # 3초세기
-        print(self.time)
-        for i in range(self.time):
-
-            # 만일 현재 돌아가는 타이머가 본인이 아니라면,
-            # 즉 내가 실행된 뒤 새로 입찰 요청이 와 타이머가 시작되었다면
-            # 숫자세기를 멈춤
-            print(self.check())
-            if self.check() is False:
-                break
-            sendMessage(client_list, str(self.time-i))
-            sleep(1)
-            print(i)
-
-        if self.check() is True:
-            is_receiving = False
-            sendMessage(client_list, "end")
-
-
-def flagkeeper():
-
-    while is_receiving is True:
-        sleep(0.05)
-
-    print('flag end')
-
-
-def sendMessage(client_list, message):
-
-    for client in client_list:
-        client.send(message)
-
-
-def connection():
-
-    global client_list
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(SERVER_ADDRESS)
-    server_socket.listen()
-
-    while len(client_list) < 2:
-
-        try:
-            client_socket, client_address = server_socket.accept()
-        except:
+            print(data)
+        except OSError:
+            print('연결이 종료되었습니다.')
             break
 
-        new_client = client(client_socket, client_address, START_MONEY)
-        client_list.append(new_client)
-
-        print("Waiting for players...({}/2)".format(len(client_list)))
-
-    print("Game Starts!")
-
-    for c in client_list:
-        # client = copy.copy(client)
-        c.start()
+    mysock.close()
 
 
-# 경매 물품 하나를 랜덤으로 선택 (반환값: 아이템 이름 문자열)
-def randomSelect():
-    random_item = random.choice(list(item_dict.keys()))
-    item_dict[random_item] -= 1
-    # 남아있는 물품이 없다면 삭제
-    if item_dict[random_item] == 0:
-        del item_dict[random_item]
-    return random_item
+# 메시지를 수신할 스레드 생성 및 실행
+thread_recv = threading.Thread(target=receive, args=())
+thread_recv.start()
+print("Started!")
 
 
-def timeOut():
-    pass
+# 메시지 전송 및 판단
+while True:
+    try:
+        data = input('>')
+    except KeyboardInterrupt:
+        break
 
+    if data == '!quit' or '':
+        break
+    elif data == 'CALL':
+        mysock.send(bytes(data, 'utf-8'))
+    else:
+        print("To Bid, enter 'CALL'")
+        continue
 
-def auctionTime():
-
-    global current_keeper
-    global call_count
-    global is_receiving
-    current_keeper = 0
-    call_count = 0
-    is_receiving = True
-
-    for client in client_list:
-        client = copy.copy(client)
-
-    rand_item = randomSelect()
-    sendMessage(client_list, "This round's item is {}".format(rand_item))
-    sendMessage(client_list, "Bidding Starts...")
-    sleep(1)
-    sendMessage(client_list, "now!")
-
-    flag = threading.Thread(target=flagkeeper)
-    flag.start()
-    flag.join()
-
-    # for client in client_list:
-    #     client.join()
-
-    print("{} won {}".format(highest_bidder, rand_item))
-    sendMessage(client_list, "{} won {}".format(highest_bidder, rand_item))
-
-
-# pragma MAIN
-def main():
-
-    connection()
-    while item_dict:
-        auctionTime()
-
-
-if __name__ == '__main__':
-    while True:
-        main()
+# 서버 접속 종료
+mysock.close()
+print("disconneted")
